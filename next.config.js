f329f939a1d930a0c9217da2fcec0f241f88542c
holard <fagbolagbenga2015@gmail.com>
2023-01-16 21:13:53 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,53 @@
 /** @type {import('next').NextConfig} */
+
+const webpack = require('webpack');
+require('dotenv').config();
+
+
+
 const nextConfig = {
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+    
+    config.plugins.push(new webpack.DefinePlugin(env));
+    return config;
+  },
+
   experimental: {
     appDir: true,
   },
-}
+};
+
 
 module.exports = nextConfig
+
+
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+
+
+
+  // const webpack = require('webpack');
+  // require('dotenv').config();
+  
+  //   module.exports = {
+  //     webpack: config => {
+  //       const env = Object.keys(process.env).reduce((acc, curr) => {
+  //         acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+  //         return acc;
+  //       }, {});
+        
+  //       config.plugins.push(new webpack.DefinePlugin(env));
+        
+  //       return config;
+  //     }
+  //   };
